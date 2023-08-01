ca0a45c4d9c5dae5f04cdaefa2d78bdc38512e5a
Adrian Tellez <gatelleza@unal.edu.co>
2023-08-01 11:25:46 -0500

    Adding some fixes to requirements.txt and in creating the first scrapping in main.py

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 url= 'https://www.pulzo.com/'
+pulzo = requests.get(url)
 
-print("Web Scrapper")
-x=1
-print(x)
+s = BeautifulSoup(pulzo.text, 'lxml')
+secciones = s.find('div', attrs ={'class':'container-temsDay'}).find_all('div', attrs ={'class':'container-tag'})
+links_secciones = [seccion.a.get("href") for seccion in secciones]
+
+sec=[]
+for link in links_secciones:
+  try:
+    sec.append(requests.get(link))
+  except Exception as e:
+    print("Error")
+    print(e)
+    print('\n')
+
+s_seccion = []
+for s_s in sec:
+  s_seccion.append(BeautifulSoup(s_s.text, 'lxml'))
+
+print(s_seccion[0].prettify)
