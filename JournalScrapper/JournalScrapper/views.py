d4e436539ccf526f9ad1385309bb445c53fc768a
Adrian Tellez <gatelleza@unal.edu.co>
2023-08-01 16:20:27 -0500

    adding templates

--- JournalScrapper/JournalScrapper/views.py
+++ JournalScrapper/JournalScrapper/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 
-from django.template import Template, Context
-
 from django.http import HttpResponse
 
 
 def index(request):
-    doc_externo = open("C:/Users/telle/OneDrive/Documentos/proyecto/pythonScrapperProject/JournalScrapper/JournalScrapper/templates/index.html")
-    plt=Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context()
-    document = plt.render(ctx)
     return render(request, 'JournalScrapper/index.html',{})
